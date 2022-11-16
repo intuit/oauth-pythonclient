5bc9ce3d448ba30e6a488804047828428c0c8c6f
Dov Benyomin Sohacheski <b@kloud.email>
2022-11-16 16:00:34 +0200

    Added python version classifiers

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 version = {}
-with open("./intuitlib/version.py") as fp:
+with open('./intuitlib/version.py') as fp:
     exec(fp.read(), version)
 
 setup(
@@ -38,5 +38,16 @@ setup(
         'enum-compat',
     ],
     license='Apache 2.0',
-    keywords='intuit quickbooks oauth auth openid client'
+    keywords='intuit quickbooks oauth auth openid client',
+    classifiers=[
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ]
 )
